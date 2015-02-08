3739d96d3d19bf47535823fa693d2e411404c2a6
Blake Embrey <hello@blakeembrey.com>
2015-02-08 15:48:37 +0900

    update code style and tests

--- test.js
+++ test.js
@@ -1,16 +1,16 @@
 /* global describe, it */
-var assert      = require('assert');
-var isUpperCase = require('./');
+var assert = require('assert')
+var isUpperCase = require('./')
 
 describe('is upper case', function () {
   it('should check if the string is upper case', function () {
-    assert.equal(isUpperCase('TEST'), true);
-    assert.equal(isUpperCase('test'), false);
-    assert.equal(isUpperCase('Test'), false);
-  });
+    assert.equal(isUpperCase('TEST'), true)
+    assert.equal(isUpperCase('test'), false)
+    assert.equal(isUpperCase('Test'), false)
+  })
 
   it('should support different locales', function () {
-    assert.equal(isUpperCase('STRİNG', 'tr'), true);
-    assert.equal(isUpperCase('STRING', 'tr'), true);
-  });
-});
+    assert.equal(isUpperCase('STRİNG', 'tr'), true)
+    assert.equal(isUpperCase('STRING', 'tr'), true)
+  })
+})
